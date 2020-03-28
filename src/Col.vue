023e0fe4cd5ae1ac45432ec88705cf676523f4c5
mxylovezh <18322276773@163.com>
2020-03-28 13:15:49 +0800

    基本实现栅格系统

--- src/Col.vue
+++ src/Col.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
+  <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
+    :style="{paddingLeft:gutter/2+'px',paddingRight:gutter/2+'px'}"
+    >
+    <div style="border: 1px solid green; height: 100px;">
     <slot></slot>
+    </div>
   </div>
 </template>
 <script>
@@ -9,21 +13,31 @@
     props: {
       span: {
         type: [Number,String]
+      },
+      offset: {
+        type: [Number,String]
+      }
+    },
+    data(){
+      return {
+        gutter: 0
       }
     }
   }
 </script>
 <style lang="scss" scoped>
   .col{
-    height: 100px;
-    background: grey;
-    border: 1px solid red;
     width: 50%;
-
     $class-prefix: col-;
     @for $n from 1 through 24{
       &.#{$class-prefix}#{$n}{
         width: ($n / 24) * 100%;
+      }
+    }
+    $class-prefix: offset-;
+    @for $n from 1 through 24{
+      &.#{$class-prefix}#{$n}{
+        margin-left: ($n / 24) * 100%;
       }
     }
   }
