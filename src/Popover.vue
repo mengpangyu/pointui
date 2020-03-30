6df7c4014cc92d7d7f5a543e668a4e48742081ad
mxylovezh <18322276773@163.com>
2020-03-30 17:50:54 +0800

    实现最简单的 popover

--- src/Popover.vue
+++ src/Popover.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="popover" @click="xx">
-    <div class="content-wrapper" v-if="visible">
+  <div class="popover" @click.stop="xx">
+    <div class="content-wrapper" v-if="visible" @click.stop>
       <slot name="content"></slot>
     </div>
     <slot></slot>
@@ -17,6 +17,15 @@
     methods:{
       xx(){
         this.visible = !this.visible
+        if(this.visible === true){
+          setTimeout(()=>{
+            let eventHandler = () =>{
+              this.visible = false
+              document.removeEventListener('click',eventHandler)
+            }
+            document.addEventListener('click', eventHandler)
+          },500)
+        }
       }
     }
   }
