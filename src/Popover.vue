d376f41fb6beb7269841ab0e68b73189ea465ee7
mxylovezh <18322276773@163.com>
2020-03-31 14:25:13 +0800

    setTimeout 和 nextTick 的选择出现了问题,回滚一下

--- src/Popover.vue
+++ src/Popover.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="popover" @click.stop="xx">
-    <div ref="contentWrapper" class="content-wrapper" v-if="visible" @click.stop>
+  <div class="popover" @click="onClick">
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
     <span ref="triggerWrapper">
@@ -11,39 +11,51 @@
 <script>
   export default {
     name: 'PointPopover',
-    data(){
+    data() {
       return {
         visible: false
       }
     },
-    methods:{
-      xx(){
-        this.visible = !this.visible
-        if(this.visible === true){
-          this.$nextTick(()=>{
-            document.body.appendChild(this.$refs.contentWrapper)
-            let {width,height,top,left} = this.$refs.triggerWrapper.getBoundingClientRect()
-            this.$refs.contentWrapper.style.left = `${left + window.scrollX}px`
-            this.$refs.contentWrapper.style.top = `${top + window.scrollY}px`
-            let eventHandler = () =>{
-              this.visible = false
-              document.removeEventListener('click',eventHandler)
-            }
-            document.addEventListener('click', eventHandler)
-          })
+    methods: {
+      onClick(event) {
+        console.log(event.target)
+        if (this.$refs.triggerWrapper.contains(event.target)) {
+          this.visible = !this.visible
+          console.log(this.visible)
+          if (this.visible === true) {
+            setTimeout(() => {
+              document.body.appendChild(this.$refs.contentWrapper)
+              let {top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+              this.$refs.contentWrapper.style.left = `${left + window.scrollX}px`
+              this.$refs.contentWrapper.style.top = `${top + window.scrollY}px`
+              let eventHandler = (e) => {
+                console.log(e.target)
+                console.log(this.$refs.contentWrapper)
+                if(!this.$refs.contentWrapper.contains(e.target)){
+                  this.visible = false
+                  document.removeEventListener('click', eventHandler)
+                }
+              }
+              document.addEventListener('click', eventHandler)
+            })
+          }else{
+            console.log('dd')
+          }
         }
       }
     }
   }
 </script>
 <style lang="scss" scoped>
-.popover{
-  display: inline-block;
-  vertical-align: top;
-  position: relative;
-}
-.content-wrapper{
-  position: absolute;
-  transform: translateY(-100%);
-}
+  .popover {
+    display: inline-block;
+    vertical-align: top;
+    position: relative;
+  }
+
+  .content-wrapper {
+    width: 50px;
+    position: absolute;
+    transform: translateY(-100%);
+  }
 </style>
