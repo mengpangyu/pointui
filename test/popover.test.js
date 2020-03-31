818ca8e93eec6f7cbaec0cb9e9e534209f818d6b
mxylovezh <18322276773@163.com>
2020-03-31 20:34:10 +0800

    有一个popover 的测试用例没有实现

---
+++ test/popover.test.js
@@ -0,0 +1,56 @@
+const expect = chai.expect;
+import Vue from 'vue'
+import Popover from '../src/popover.vue'
+
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Popover', () => {
+  it('存在.', () => {
+    expect(Popover).to.be.ok
+  })
+  it('可以设置 position', (done) => {
+    Vue.component('p-popover', Popover)
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    div.innerHTML = `
+    <p-popover position="left" ref="a">
+      <template slot="content">
+      弹出内容
+      </template>
+      <button>下面</button>
+    </p-popover>
+    `
+    const vm = new Vue({
+      el: div
+    })
+    vm.$el.querySelector('button').click()
+    vm.$nextTick(() => {
+      expect(vm.$refs.a.$refs.contentWrapper.classList.contains('position-left')).to.be.true
+      done()
+    })
+  })
+  xit('可以设置 trigger', (done) => {
+    Vue.component('p-popover', Popover)
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    div.innerHTML = `
+    <p-popover trigger="hover" ref="a">
+      <template slot="content">
+      弹出内容
+      </template>
+      <button>下面</button>
+    </p-popover>
+    `
+    const vm = new Vue({
+      el: div
+    })
+    let event = new Event('mouseenter')
+    vm.$el.dispatchEvent(event)
+    vm.$nextTick(() => {
+      expect(vm.$refs.a.$refs.contentWrapper).to.be.exist
+      done()
+    })
+  })
+})
